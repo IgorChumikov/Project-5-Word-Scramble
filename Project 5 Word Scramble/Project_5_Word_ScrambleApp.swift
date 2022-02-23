6afb1f1d053179bc3480194458761df423ffef66
IgorChumikov <chumikov.igor@mail.ru>
2022-02-23 21:11:11 +0300

    Initial Commit

---
+++ Project 5 Word Scramble/Project_5_Word_ScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_5_Word_ScrambleApp.swift
+//  Project 5 Word Scramble
+//
+//  Created by Игорь Чумиков on 23.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct Project_5_Word_ScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
